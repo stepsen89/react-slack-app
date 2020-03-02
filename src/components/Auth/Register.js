303494e1316f8c05c8866e424ca5e46b329100eb
stepsen89 <stephanie.senoner@gmail.com>
2020-03-02 11:17:05 +0000

    register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+
+import {Link} from "react-router-dom"
 
 export class Register extends Component {
     render() {
         return (
-            <div>
-                Register
-            </div>
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as="h2" icon color="blue" textAlign="center">
+                        <Icon name="signup" color="blue" />
+                        Register
+                    </Header>
+                    <Form size="large">
+                        <Segment stacked padded="very">
+                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text"/>
+                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="E-Mail Address" onChange={this.handleChange} type="email"/>
+                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password"/>
+                            <Form.Input fluid name="passwordConfirmation" icon="lock" iconPosition="left" placeholder="Confirm your password" onChange={this.handleChange} type="password"/>
+                            <Button color="blue" fluid size="large">Submit</Button> 
+                        </Segment>
+                    </Form>
+                    <Message> Already a user? <Link to="/login"> Login </Link> </Message>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+// importing sea
+import 'semantic-ui-css/semantic.min.css'
+
 
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
