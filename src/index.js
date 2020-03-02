0a344c3842312525a5de8b7337b306948d53d2ee
stepsen89 <stephanie.senoner@gmail.com>
2020-03-02 10:42:22 +0000

    setting up routes using Switch, Route and exact keyword

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route path="/login" component={Login}/>
+            <Route path="/register" component={Register}/>
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 serviceWorker.unregister();
