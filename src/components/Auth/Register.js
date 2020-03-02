23e5e70b6ea86823ebd8f40bd5c26554a6320804
stepsen89 <stephanie.senoner@gmail.com>
2020-03-02 11:52:53 +0000

    cleaned up register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+import { Grid, Form, Segment, Button, Header, Message } from 'semantic-ui-react';
 
 import {Link} from "react-router-dom"
 
@@ -7,17 +7,16 @@ export class Register extends Component {
     render() {
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
-                <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="blue" textAlign="center">
-                        <Icon name="signup" color="blue" />
-                        Register
-                    </Header>
-                    <Form size="large">
+                <Grid.Column style={{ maxWidth: 500 }}>
+                    <Form size="big">
                         <Segment stacked padded="very">
-                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text"/>
-                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="E-Mail Address" onChange={this.handleChange} type="email"/>
-                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password"/>
-                            <Form.Input fluid name="passwordConfirmation" icon="lock" iconPosition="left" placeholder="Confirm your password" onChange={this.handleChange} type="password"/>
+                    <Header as="h2" color="blue" textAlign="center">
+                        Sign Up
+                    </Header>
+                            <Form.Input fluid size="big" name="username" icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text"/>
+                            <Form.Input fluid size="big" name="email" icon="mail" iconPosition="left" placeholder="E-Mail Address" onChange={this.handleChange} type="email"/>
+                            <Form.Input fluid size="big" name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password"/>
+                            <Form.Input fluid size="big" name="passwordConfirmation" icon="lock" iconPosition="left" placeholder="Confirm your password" onChange={this.handleChange} type="password"/>
                             <Button color="blue" fluid size="large">Submit</Button> 
                         </Segment>
                     </Form>
