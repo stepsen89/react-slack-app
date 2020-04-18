dee737fe2a0d129f8f91a84a29363f430f4c3ca2
stepsen89 <stephanie.senoner@gmail.com>
2020-04-18 22:51:13 +0100

    fixed index.js routing issue withAuth

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ import "semantic-ui-css/semantic.min.css";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 
 // @Notes
 // using the exact keyword is necessary to match routes
@@ -24,18 +29,21 @@ class Root extends Component {
   }
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
     );
   }
 }
 
-const RoutWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(Root);
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
