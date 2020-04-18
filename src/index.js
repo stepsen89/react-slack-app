66819115255e05bcbe1487df8bc114f4eeeab320
stepsen89 <stephanie.senoner@gmail.com>
2020-04-18 22:48:19 +0100

    changes index.js to listen to firebase auth

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "./firebase";
 
 // importing sea
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // @Notes
 // using the exact keyword is necessary to match routes
-const Root = () => (
-    <Router>
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return (
+      <Router>
         <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
-    </Router>
-)
+      </Router>
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RoutWithAuth = withRouter(Root);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 serviceWorker.unregister();
