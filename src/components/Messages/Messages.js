1325e5bc188bb894ce43496c4e61bb73a7a3b39a
stepsen89 <stephanie.senoner@gmail.com>
2020-11-04 17:05:11 +0000

    messages, header and form components

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 class Messages extends Component {
   render() {
     return (
-      <div>
-        Messages
-      </div>
+      <Fragment>
+        <MessagesHeader />
+        <Segment>
+          <Comment.Group className="messages">
+
+          </Comment.Group>
+        </Segment>
+        <MessageForm />
+      </Fragment>
     )
   }
 }
